60a8c44a55d4f46b5b71278611f3e2c825fdd302
Happiness Maina <mainahappiness361@gmail.com>
2024-11-29 10:53:03 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+
+ function distanceFromHqInBlocks(streetNumber){
+    return Math.abs(42-streetNumber);
+        
+    }
+
+    function distanceTravelledInFeet(start, destination) {
+      const feetPerBlock = 264;
+      return Math.abs(destination - start) * feetPerBlock;
+  }
+function distanceFromHqInFeet(location){
+  const feetPerBlock= 264;
+  const Hq=42;
+  return Math.abs(location-Hq)*264;
+}  
+
+ function calculatesFarePrice(start, destination) {
+     const feetPerBlock = 264;
+    const distance = Math.abs(destination - start) * feetPerBlock;
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return parseFloat((distance - 400) * 0.02);
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
+
+
+    
+    
